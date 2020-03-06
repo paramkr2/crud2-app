2e6712d211e4038d0dbb811cf6b4686ebe0da278
amankuu <kumaramanku0@gmail.com>
2020-03-07 00:22:46 +0530

    Updates

---
+++ precache-manifest.70f11d2da4d7cbc8aaa2d94048c1cbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea90e32d06b0c391f13ea975a7261219",
+    "url": "/crud2-app/index.html"
+  },
+  {
+    "revision": "c8a7bee4b9afb1e15680",
+    "url": "/crud2-app/static/css/main.0441f6cd.chunk.css"
+  },
+  {
+    "revision": "e1239b50d2d6b4e3b51a",
+    "url": "/crud2-app/static/js/2.16d4cdc5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/crud2-app/static/js/2.16d4cdc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a7bee4b9afb1e15680",
+    "url": "/crud2-app/static/js/main.9464d352.chunk.js"
+  },
+  {
+    "revision": "c8139e553ba01df7cb1c",
+    "url": "/crud2-app/static/js/runtime-main.5e41719b.js"
+  }
+]);
